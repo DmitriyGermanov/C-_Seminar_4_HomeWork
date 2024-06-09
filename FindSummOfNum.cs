2a6ce27f6544c54833798f60f88249b68852170a
Dmitriy Germanov <seltis.gtn@gmail.com>
2024-06-09 19:00:32 +0300

    initial commit

---
+++ FindSummOfNum.cs
@@ -0,0 +1,29 @@
+﻿namespace HomeWork
+{
+    internal class FindSummOfNum
+    {
+        public Tuple<int, int, int> FindNumbers(int[] array, int num)
+        {
+            Dictionary<int, int[]> pairs = new Dictionary<int, int[]>();
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    int sum = array[i] + array[j];
+                    if (!pairs.ContainsKey(sum))
+                    {
+                        pairs.Add(sum, new int[] { i, j });
+                    }
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (pairs.ContainsKey(num - array[i]) && pairs[num - array[i]][0] != i && pairs[num - array[i]][1] != i)
+                {
+                    return new Tuple<int, int, int>(i, pairs[num - array[i]][0], pairs[num - array[i]][1]);
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Дан массив и число. Найдите три числа в массиве сумма 
+    которых равна искомому числу. Подсказка: если взять 
+    первое число в массиве, можно ли найти в оставшейся его
+    части два числа равных по сумме первому.
+*/
+
+using HomeWork;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        FindSummOfNum findNumm = new();
+        int[] array = { 1, 2, 5, 7, 3, 4, 2 };
+        int num = 11;
+        var result = findNumm.FindNumbers(array, num);
+        if (result != null)
+            Console.WriteLine($"Позиция элемента в массиве: {result.ToString()}");
+        else
+            Console.WriteLine("Комбинация чисел не найдена");
+    }
+}
